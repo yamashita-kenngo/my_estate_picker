ea999b69a0a27de7e6527415117273e6d4303e54
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2020-09-13 23:51:05 +0900

    add OpenAPI to App

--- app/apps/api/src/main.ts
+++ app/apps/api/src/main.ts
@@ -1,10 +1,18 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('estate data')
+    .setDescription('The estates API description')
+    .setVersion('0.1')
+    .addTag('estates')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
